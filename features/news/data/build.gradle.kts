156e16cf8ef1bb15253408145b2df051da750ba1
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-15 19:33:30 +0530

    [FEAT]: Added news data module

--- features/news/data/build.gradle.kts
+++ features/news/data/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -35,10 +37,28 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    with(Modules) {
+        implementation(project(mapOf("path" to coreNetwork)))
+    }
+    with(Dependencies.Retrofit) {
+        implementation(retrofit)
+        implementation(gson)
+        implementation(gsonConverter)
+    }
+    with(Dependencies.OkHttp) {
+        implementation(okhttp)
+        implementation(loggingInterceptor)
+    }
+
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-compiler:2.44")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+kapt {
+    correctErrorTypes = true
+}
+
