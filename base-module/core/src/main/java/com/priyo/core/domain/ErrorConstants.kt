d74348069867577af5a63aaf480da7d2bc549493
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-15 21:22:30 +0530

    [FEAT]: Added error constants

---
+++ base-module/core/src/main/java/com/priyo/core/domain/ErrorConstants.kt
@@ -0,0 +1,7 @@
+package com.priyo.core.domain
+
+object ErrorConstants {
+    const val SOMETHING_WENT_WRONG = "Something went wrong"
+    const val NO_INTERNET = "No Internet Connection"
+    const val UNKNOWN_ERROR = "Unknown Error"
+}
