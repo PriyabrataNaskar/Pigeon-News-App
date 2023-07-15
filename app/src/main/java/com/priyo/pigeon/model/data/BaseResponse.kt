603c6e66168f5ebda88f2af324a7e3c7167c2cff
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-15 18:50:54 +0530

    [FEAT]: Added serialized name annotation on responses

--- app/src/main/java/com/priyo/pigeon/model/data/BaseResponse.kt
+++ app/src/main/java/com/priyo/pigeon/model/data/BaseResponse.kt
@@ -1,7 +1,12 @@
 package com.priyo.pigeon.model.data
 
+import com.google.gson.annotations.SerializedName
+
 data class BaseResponse<T>(
+    @SerializedName("articles")
     val list: List<T>,
+    @SerializedName("status")
     val status: String,
+    @SerializedName("totalResults")
     val totalResults: Int,
 )
