c9744f5debe6cbce576b9e71fb7732035d0dfe92
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-15 19:34:44 +0530

    [FEAT]: Added module name constants

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,7 @@
+object Modules {
+    const val app = ":app"
+    const val core= ":base-module:core"
+    const val coreUi = ":base-module:core-ui"
+    const val coreNetwork = ":base-module:core-network"
+    const val newsData = ":features:news:data"
+}
