5647bc0bf0b78f42e47929e84943c1eadcff811a
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-15 19:34:14 +0530

    [FEAT]: Added news data classes

---
+++ features/news/data/src/main/java/com/priyo/news/data/source/dto/ArticleDto.kt
@@ -0,0 +1,22 @@
+package com.priyo.news.data.source.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class ArticleDto(
+    @SerializedName("author")
+    val author: String?,
+    @SerializedName("content")
+    val content: String?,
+    @SerializedName("description")
+    val description: String?,
+    @SerializedName("publishedAt")
+    val publishedAt: String?,
+    @SerializedName("source")
+    val source: SourceDto,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("url")
+    val url: String?,
+    @SerializedName("urlToImage")
+    val urlToImage: String?,
+)
---
+++ features/news/data/src/main/java/com/priyo/news/data/source/dto/SourceDto.kt
@@ -0,0 +1,10 @@
+package com.priyo.news.data.source.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class SourceDto(
+    @SerializedName("id")
+    val id: String?,
+    @SerializedName("name")
+    val name: String?,
+)
