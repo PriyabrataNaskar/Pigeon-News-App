cff815d67450f1dcd3b993777239f82bacad0655
PriyabrataSeemai <priyabrata.naskar@semaai.com>
2023-07-16 13:12:30 +0530

    [FEAT]: 1. Added lifecycle dependency

--- base-module/core-ui/build.gradle.kts
+++ base-module/core-ui/build.gradle.kts
@@ -36,4 +36,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
 }
