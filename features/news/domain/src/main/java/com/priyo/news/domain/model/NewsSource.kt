c1b20947567e9285dddbd8c01b80b88d871c36a1
-l <naskar.priyabrata66@gmail.com>
2023-08-07 21:52:54 +0530

    [FEAT]: 1. Make NewsSource Percalable

--- features/news/domain/src/main/java/com/priyo/news/domain/model/NewsSource.kt
+++ features/news/domain/src/main/java/com/priyo/news/domain/model/NewsSource.kt
@@ -1,6 +1,10 @@
 package com.priyo.news.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class NewsSource(
     val id: String?,
     val name: String?,
-)
+) : Parcelable
