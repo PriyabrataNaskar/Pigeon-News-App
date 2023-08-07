a6841725bd6d150d0546507c9e09744d27501bd4
-l <naskar.priyabrata66@gmail.com>
2023-08-07 21:46:52 +0530

    [FEAT]: 1. Added Diffutils 2. Updated Adapter 3. Added ViewHolder

---
+++ features/news/ui/src/main/java/com/priyo/news/ui/adapter/NewsDiffUtils.kt
@@ -0,0 +1,65 @@
+package com.priyo.news.ui.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+import com.priyo.news.domain.model.Article
+
+class NewsDiffUtils(
+    private val oldList: List<Article>,
+    private val newList: List<Article>,
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(
+        oldItemPosition: Int,
+        newItemPosition: Int,
+    ): Boolean {
+        return oldList.get(oldItemPosition) == newList.get(
+            newItemPosition,
+        )
+    }
+
+    override fun areContentsTheSame(
+        oldItemPosition: Int,
+        newItemPosition: Int,
+    ): Boolean {
+        return when {
+            oldList.get(oldItemPosition).author != newList.get(
+                newItemPosition,
+            ).author -> {
+                false
+            }
+
+            oldList.get(oldItemPosition).content != newList.get(
+                newItemPosition,
+            ).content -> {
+                false
+            }
+
+            oldList.get(oldItemPosition).title != newList.get(
+                newItemPosition,
+            ).title -> {
+                false
+            }
+
+            oldList.get(oldItemPosition).url != newList.get(
+                newItemPosition,
+            ).url -> {
+                false
+            }
+
+            oldList.get(oldItemPosition).source != newList.get(
+                newItemPosition,
+            ).source -> {
+                false
+            }
+
+            else -> true
+        }
+    }
+}
---
+++ features/news/ui/src/main/java/com/priyo/news/ui/adapter/NewsViewHolder.kt
@@ -0,0 +1,60 @@
+package com.priyo.news.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.priyo.news.domain.model.Article
+import com.priyo.news.ui.R
+import com.priyo.news.ui.databinding.ItemNewsCardBinding
+
+class NewsViewHolder(
+    private val binding: ItemNewsCardBinding,
+    private val onItemClick: ((Article, Int) -> Unit)?,
+    private val onShareIconClick: ((Article) -> Unit)?,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+        fun create(
+            viewGroup: ViewGroup,
+            onItemClick: ((Article, Int) -> Unit)?,
+            onShareIconClick: ((Article) -> Unit)?,
+        ): NewsViewHolder {
+            return NewsViewHolder(
+                ItemNewsCardBinding.inflate(
+                    LayoutInflater.from(viewGroup.context),
+                    viewGroup,
+                    false,
+                ),
+                onItemClick,
+                onShareIconClick,
+            )
+        }
+    }
+
+    fun bindData(article: Article, position: Int) {
+        setView(article)
+        setOnClickListener(article, position)
+    }
+
+    private fun setView(article: Article) {
+        with(binding) {
+            newsTitle.text = article.title
+            authorNameText.text = article.author
+            binding.newsImage
+            binding.descriptionText.text = article.description
+            Glide.with(binding.root.context).load(article.urlToImage)
+                .placeholder(R.drawable.ic_placeholder_image)
+                .centerCrop().into(newsImage)
+        }
+    }
+
+    private fun setOnClickListener(article: Article, position: Int) {
+        binding.shareButton.setOnClickListener {
+            onShareIconClick?.invoke(article)
+        }
+        binding.root.setOnClickListener {
+            onItemClick?.invoke(article, position)
+        }
+    }
+}
