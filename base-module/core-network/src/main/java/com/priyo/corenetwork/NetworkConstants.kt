24d33bd8a72f45e707449a68b1e5328f3e2b3bba
-l <naskar.priyabrata66@gmail.com>
2023-10-08 16:12:57 +0530

    [FEAT]: 1. Remove unused constants

--- base-module/core-network/src/main/java/com/priyo/corenetwork/NetworkConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.priyo.corenetwork
-
-object NetworkConstants {
-    const val SOMETHING_WENT_WRONG = "Something went wrong"
-    const val NO_INTERNET = "No Internet Connection"
-    const val UNKNOWN_ERROR = "Unknown Error"
-}
