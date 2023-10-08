050deff034f466f27c2d8a37a687e33079a643e8
-l <naskar.priyabrata66@gmail.com>
2023-10-08 16:10:44 +0530

    [FEAT]: 1. remove unused methods

--- base-module/core/src/main/java/com/priyo/core/domain/coroutine/CoroutineDispatcherProvider.kt
+++ base-module/core/src/main/java/com/priyo/core/domain/coroutine/CoroutineDispatcherProvider.kt
@@ -1,11 +1,8 @@
 package com.priyo.core.domain.coroutine
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 
 interface CoroutineDispatcherProvider {
     val dispatcherDefault: CoroutineDispatcher
     val dispatcherIO: CoroutineDispatcher
-    suspend fun <T> switchToDefault(block: suspend CoroutineScope.() -> T): T
-    suspend fun <T> switchToIO(block: suspend CoroutineScope.() -> T): T
 }
--- base-module/core/src/main/java/com/priyo/core/domain/coroutine/CoroutineDispatcherProviderImpl.kt
+++ base-module/core/src/main/java/com/priyo/core/domain/coroutine/CoroutineDispatcherProviderImpl.kt
@@ -1,23 +1,9 @@
 package com.priyo.core.domain.coroutine
 
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 class CoroutineDispatcherProviderImpl : CoroutineDispatcherProvider {
     override val dispatcherDefault: CoroutineDispatcher = Dispatchers.Default
     override val dispatcherIO: CoroutineDispatcher = Dispatchers.IO
-
-    override suspend fun <T> switchToDefault(
-        block: suspend CoroutineScope.() -> T,
-    ): T =
-        withContext(dispatcherDefault) {
-            block()
-        }
-
-    override suspend fun <T> switchToIO(block: suspend CoroutineScope.() -> T): T =
-        withContext(dispatcherIO) {
-            block()
-        }
 }
