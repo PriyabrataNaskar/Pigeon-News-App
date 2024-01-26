72f872d8c2ee80319176bb183603880f6e4f3200
-l <naskar.priyabrata66@gmail.com>
2024-01-26 22:25:11 +0530

    [FEAT]: Init analytics

--- app/src/main/java/com/priyo/pigeon/PigeonApplication.kt
+++ app/src/main/java/com/priyo/pigeon/PigeonApplication.kt
@@ -1,7 +1,23 @@
 package com.priyo.pigeon
 
 import android.app.Application
+import com.priyo.analytics.AnalyticsInitialization
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 
 @HiltAndroidApp
-class PigeonApplication : Application()
+class PigeonApplication : Application() {
+
+    private lateinit var analyticsInitialization: AnalyticsInitialization
+    override fun onCreate() {
+        super.onCreate()
+
+        analyticsInitialization = AnalyticsInitialization(
+            coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            context = applicationContext,
+        )
+        analyticsInitialization.init()
+    }
+}
